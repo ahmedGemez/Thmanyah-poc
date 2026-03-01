901e1b6321744bc85f7f37b738adae85c631e08e
ahmedgaber <e-a.gaber@lean.sa>
2026-03-01 22:26:51 +0300

    searchUseCase unit test

---
+++ domain/src/test/java/com/thmanyah/domain/usecases/SearchUseCaseTest.kt
@@ -0,0 +1,62 @@
+package com.thmanyah.domain.usecases
+
+import com.thmanyah.domain.models.SearchResponse
+import com.thmanyah.domain.models.Section
+import com.thmanyah.domain.repositories.HomeRepo
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import javax.naming.directory.SearchResult
+
+class SearchUseCaseTest {
+
+    private lateinit var homeRepo: HomeRepo
+    private lateinit var searchUseCase: SearchUseCase
+
+    @Before
+    fun setup() {
+        homeRepo = mockk()
+        searchUseCase = SearchUseCase(homeRepo)
+    }
+
+    @Test
+    fun `invoke should return search response from repository`() = runTest {
+        // Given
+        val expectedResponse = SearchResponse(
+             listOf(
+                 Section(
+                    name = "1",
+                     type = "Test type",
+                     contentType = "Test contentType"
+                )
+            )
+        )
+        coEvery { homeRepo.search() } returns flowOf(expectedResponse)
+
+        // When
+        val result = searchUseCase().first()
+
+        // Then
+        assertEquals(expectedResponse, result)
+    }
+
+    @Test
+    fun `invoke should return empty search response when repository returns empty results`() = runTest {
+        // Given
+        val expectedResponse = SearchResponse(emptyList())
+        coEvery { homeRepo.search() } returns flowOf(expectedResponse)
+
+        // When
+        val result = searchUseCase().first()
+
+        // Then
+        assertEquals(expectedResponse, result)
+        assertEquals(0, result.sections?.size)
+    }
+}
+
