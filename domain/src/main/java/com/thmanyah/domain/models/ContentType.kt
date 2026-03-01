a13b5a3138d23366033a1b744e0d912ab03ee976
ahmedgemez <agaber929@gmail.com>
2026-03-01 22:26:51 +0300

    add enums for sections and ContentType

---
+++ domain/src/main/java/com/thmanyah/domain/models/ContentType.kt
@@ -0,0 +1,8 @@
+package com.thmanyah.domain.models
+
+enum class ContentType {
+    PODCAST,
+    EPISODE,
+    AUDIO_BOOK,
+    AUDIO_ARTICLE
+} 
---
+++ domain/src/main/java/com/thmanyah/domain/models/SectionType.kt
@@ -0,0 +1,8 @@
+package com.thmanyah.domain.models
+
+enum class SectionType {
+    SQUARE,
+    TWO_LINES_GRID,
+    BIG_SQUARE,
+    QUEUE
+} 
